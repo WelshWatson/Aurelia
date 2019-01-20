11ff46f4072856aae946d9f030e180a11fd5557c
Owen Watson <owen_watson84@hotmail.com>
2019-01-20 16:08:29 +0000

    Added routing, home and about pages

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
+import { PLATFORM } from 'aurelia-framework';
+import {RouterConfiguration, Router} from 'aurelia-router';
+  
 export class App {
-  message = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router): void {
+    this.router = router;
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'home'],       name: 'home',       moduleId: PLATFORM.moduleName('home/home-page') },
+      { route: ['about'],          name: 'about',      moduleId: PLATFORM.moduleName('about/about') }
+    ]);
+  }
 }
+
+
