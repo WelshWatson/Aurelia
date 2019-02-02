547a5e520467578e3ed96ded95428e61ba58f4ea
Owen Watson <owen_watson84@hotmail.com>
2019-02-02 23:07:03 +0000

    Added parent and child pages with variable binding

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,9 @@ export class App {
     config.title = 'Aurelia';
     config.map([
       { route: ['', 'home'],       name: 'home',       moduleId: PLATFORM.moduleName('home/home-page') },
-      { route: ['about'],          name: 'about',      moduleId: PLATFORM.moduleName('about/about') }
+      { route: ['about'],          name: 'about',      moduleId: PLATFORM.moduleName('about/about') },
+      { route: ['parent'],         name: 'parent',     moduleId: PLATFORM.moduleName('parent-page/parent')},
+      { route: ['child'],          name: 'child',      moduleId: PLATFORM.moduleName('child-page/child')}
     ]);
   }
 }
